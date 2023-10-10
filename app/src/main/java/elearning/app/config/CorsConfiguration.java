404c499905762b464cb83e8680d204d7d9b63958
Lejla Omerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2023-10-10 14:45:51 +0200

    options

--- app/src/main/java/elearning/app/config/CorsConfiguration.java
+++ app/src/main/java/elearning/app/config/CorsConfiguration.java
@@ -4,20 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Path;
-
 @Configuration
 public class CorsConfiguration {
     private static final String GET = "GET";
     private static final String POST = "POST";
     private static final String DELETE = "DELETE";
     private static final String PUT = "PUT";
-    private static final String PATCH = "PATCH";
-    public WebMvcConfigurer corsConfigurer(){
+    private static final String OPTIONS = "OPTIONS";
+
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods(GET,POST,DELETE,PUT, PATCH).allowedHeaders("*").allowedOriginPatterns("*").allowCredentials(true);
+                registry.addMapping("/**").allowedMethods(GET, POST, DELETE, PUT, OPTIONS).allowedHeaders("*").allowedOriginPatterns("*")
+                        .allowCredentials(true);
             }
         };
     }
